db33aaa09632bf098665d115991a250d7b8ceaf8
Skhsouravhalder <skhsouravhalder@gmail.com>
2026-05-26 23:16:45 +0300

    update tsx package

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,7 @@ import { createServer, IncomingMessage, Server } from "http";
 const server :Server = createServer((req:IncomingMessage, res) => {
   console.log(req);
 });
+
+server.listen(5000,()=>{
+    console.log("server is fly on the port 5000")
+})

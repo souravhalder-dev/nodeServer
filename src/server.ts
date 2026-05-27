d1a2e6ede65eb88647d92be776c1d00671968d28
Skhsouravhalder <skhsouravhalder@gmail.com>
2026-05-27 13:20:10 +0600

    res and req

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server :Server = createServer((req:IncomingMessage, res) => {
-  console.log(req);
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/" && method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "this is root route" }));
+    } else if (url?.startsWith("/product")) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "this is product  route" }));
+    } else {
+      res.writeHead(404, { "content-type": "text/plain" });
+      res.end("route not found");
+    }
+  },
+);
+
+server.listen(5000, () => {
+  console.log("server is fly on the port 5000");
 });
-
-server.listen(5000,()=>{
-    console.log("server is fly on the port 5000")
-})
